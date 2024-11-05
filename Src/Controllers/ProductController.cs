ca9238ce11c9daa3ef73f2f6b370dd2b523613f0
Hvmnn <felipe.hamen@alumnos.ucn.cl>
2024-11-05 00:33:44 -0300

    feat: add endpoints in ProductController

--- Src/Controllers/ProductController.cs
+++ Src/Controllers/ProductController.cs
@@ -1,3 +1,77 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using tallerBackendIDWM.Src.Interfaces;
+using tallerBackendIDWM.Src.Models;
+
 namespace tallerBackendIDWM.Src.Controllers{
-    
+    [Authorize(Roles = "Administrador")]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var products = await _productRepository.GetProductsAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProductById(int id)
+        {
+            var product = await _productRepository.GetProductById(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateProduct(Product product)
+        {
+            await _productRepository.CreateProductAsync(product);
+            return CreatedAtAction(nameof(GetProductById), new { id = product.Id }, product);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateProduct(int id, Product product)
+        {
+            if (id != product.Id)
+            {
+                return BadRequest();
+            }
+
+            var productToUpdate = await _productRepository.GetProductById(id);
+
+            if (productToUpdate == null)
+            {
+                return NotFound();
+            }
+
+            await _productRepository.UpdateProductAsync(product);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteProduct(int id)
+        {
+            var product = await _productRepository.GetProductById(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            await _productRepository.DeleteProductAsync(id);
+            return NoContent();
+        }
+    }
+
 }
