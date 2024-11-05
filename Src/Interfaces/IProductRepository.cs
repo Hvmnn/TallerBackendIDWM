aa079c33b05c0681f07801817dcc57c0bcfc591e
Hvmnn <felipe.hamen@alumnos.ucn.cl>
2024-11-05 00:25:48 -0300

    feat: Add tasks in IProductRepository and implement tasks in Product Repository

--- Src/Interfaces/IProductRepository.cs
+++ Src/Interfaces/IProductRepository.cs
@@ -1,3 +1,12 @@
+using tallerBackendIDWM.Src.Models;
+
 namespace tallerBackendIDWM.Src.Interfaces{
-    
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<Product> GetProductById(int id);
+        Task CreateProductAsync(Product product);
+        Task UpdateProductAsync(Product product);
+        Task DeleteProductAsync(int id);
+    }
 }
--- Src/Repositories/ProductRepository.cs
+++ Src/Repositories/ProductRepository.cs
@@ -1,3 +1,45 @@
+using tallerBackendIDWM.Src.Models;
+
 namespace tallerBackendIDWM.Src.Repositories{
-    
+    public class ProductRepository : IProductRepository
+    {
+        private readonly DataContext _context;
+
+        public ProductRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Product>> GetProductsAsync()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        public async Task<Product> GetProductById(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+
+        public async Task CreateProductAsync(Product product)
+        {
+            await _context.Products.AddAsync(product);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateProductAsync(Product product)
+        {
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteProductAsync(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
 }
