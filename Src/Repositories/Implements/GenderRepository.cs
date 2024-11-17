01b2c6f61267f73fc8ed11a0c7da15615a156719
Thetrolxs <moralesharnisch@gmail.com>
2024-11-17 13:57:15 -0300

    feat: add Gender and Role repository

--- Src/Repositories/Implements/GenderRepository.cs
+++ Src/Repositories/Implements/GenderRepository.cs
@@ -2,11 +2,31 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using tallerBackendIDWM.Src.Data;
+using TallerBackendIDWM.Src.Models;
 
 namespace TallerBackendIDWM.Src.Repositories.Implements
 {
-    public class GenderRepository
+    public class GenderRepository : IGenderRepository
     {
-        
+        private readonly DataContext _context;
+        public GenderRepository(DataContext dataContext){
+            _context = dataContext;
+        }
+        public async Task<IEnumerable<Gender>> GetGenders()
+        {
+            var genders = await _context.Genders.ToListAsync();
+            return genders;
+        }
+
+        public async Task<bool> ValidateGenderId(int id)
+        {
+            var existingGender = await _context.Genders.FindAsync(id);
+            if(existingGender == null){
+                return false;
+            }
+            return true;
+        }
     }
 }
--- Src/Repositories/Implements/RoleRepository.cs
+++ Src/Repositories/Implements/RoleRepository.cs
@@ -1,12 +1,36 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using tallerBackendIDWM.Src.Data;
+using TallerBackendIDWM.Src.Models;
 
 namespace TallerBackendIDWM.Src.Repositories.Implements
 {
-    public class RoleRepository
+    public class RoleRepository : IRoleRepository
     {
-        
+        private readonly DataContext _context;
+        public RoleRepository(DataContext dataContext){
+            _context = dataContext;
+        }
+
+        public async Task<Role?> GetRoleById(int id)
+        {
+            var role = await _context.Roles.FindAsync(id);
+            return role;
+        }
+
+        public async Task<Role?> GetRoleByName(string type)
+        {
+            var role = await _context.Roles.Where(r => r.Type == type).FirstOrDefaultAsync();
+            return role;
+        }
+
+        public async Task<bool> ValidateRoleId(int id)
+        {
+            var role = await _context.Roles.FindAsync(id);
+            if(role == null){
+                return false;
+            }
+            return true;
+        }
     }
 }
