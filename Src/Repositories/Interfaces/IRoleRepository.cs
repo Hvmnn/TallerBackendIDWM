a0a399bdda85f5720de1c808abb088602bf1fa0b
Thetrolxs <moralesharnisch@gmail.com>
2024-11-17 13:55:04 -0300

    fix: Interface Role if Role is null

--- Src/Repositories/Interfaces/IRoleRepository.cs
+++ Src/Repositories/Interfaces/IRoleRepository.cs
@@ -6,7 +6,7 @@ namespace TallerBackendIDWM.Src.Repositories.Implements
     public interface IRoleRepository
     {
         Task<bool> ValidateRoleId(int id);
-        Task<Role> GetRoleById(int id);
-        Task<Role> GetRoleByName(string type);
+        Task<Role?> GetRoleById(int id);
+        Task<Role?> GetRoleByName(string type);
     }
 }
