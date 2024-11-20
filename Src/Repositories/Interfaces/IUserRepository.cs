efdca86c734fc0d6eff6497f4508d18f7f951a55
Thetrolxs <moralesharnisch@gmail.com>
2024-11-20 20:26:19 -0300

    feat: add user interface

---
+++ Src/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TallerBackendIDWM.Src.Models;
+
+namespace TallerBackendIDWM.Src.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetUsers();
+        Task<IEnumerable<User>> SearchUser(string query);
+        Task<User?> GetUserById(int id);
+        Task<User?> GetUserByEmail(string email);
+        Task<bool> VerifyRut(string rut);
+        Task<bool> AddUser(User user);
+        Task<bool> EditUser(int id);
+        Task<bool> DelUser(int id);
+        Task<bool>ChangeUserState(int id, bool userStatus);
+        Task<bool>ChangePassword(int id, string newPassword);
+    }
+}
