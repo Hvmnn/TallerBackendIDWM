3786b2edeb02313a3fdd3e1d031727aa907fb3c8
Hvmnn <felipe.hamen@alumnos.ucn.cl>
2024-11-23 15:36:46 -0300

    fix: relation configuration in DbContext.

--- Src/Data/DataContext.cs
+++ Src/Data/DataContext.cs
@@ -12,5 +12,21 @@ namespace TallerBackendIDWM.Src.Data
         public DbSet<Gender> Genders {get; set;} = null!;
         public DbSet<Role> Roles {get; set;} = null!;
         public DbSet<User> Users {get; set;} = null!;
+        public DbSet<CartItem> CartItems {get; set;} = null!;
+        public DbSet<ShoppingCart> ShoppingCarts {get; set;} = null!;
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<CartItem>()
+                .HasOne(c => c.Product)
+                .WithMany()
+                .HasForeignKey(c => c.ProductId);
+
+            modelBuilder.Entity<ShoppingCart>()
+                .HasMany(c => c.CartItems)
+                .WithOne()
+                .HasForeignKey(c => c.Id);
+        }
     }
 }
