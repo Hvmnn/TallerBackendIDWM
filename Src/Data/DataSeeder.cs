17be82bf43b970f38fcafeb7df2792eafe90eb82
Thetrolxs <moralesharnisch@gmail.com>
2024-11-22 22:50:50 -0300

    feat: add fake user at data seeder

--- Src/Data/DataSeeder.cs
+++ Src/Data/DataSeeder.cs
@@ -1,3 +1,4 @@
+using Bogus;
 using Microsoft.EntityFrameworkCore;
 using TallerBackendIDWM.Src.Data;
 using TallerBackendIDWM.Src.Models;
@@ -96,9 +97,65 @@ namespace TallerBackendIDWMallerBackendIDWM.Src.Data
                     generatedRuts.Add(administrador.Rut);
                     context.Users.Add(administrador);
 
+                    var faker = new Faker<User>()
+                    .RuleFor(u => u.Rut, f => GenerateUniqueRandomRut(generatedRuts))
+                    .RuleFor(u => u.Name, f => f.Person.FullName)
+                    .RuleFor(u => u.Birthday, f => f.Date.Past(30, DateTime.Now.AddYears(-18)))
+                    .RuleFor(u => u.Email, f => f.Internet.Email())
+                    .RuleFor(u => u.Password, f => BCrypt.Net.BCrypt.HashPassword("password"))
+                    .RuleFor(u => u.IsActive, f => f.Random.Bool())
+                    .RuleFor(u => u.RoleId, f => 2)
+                    .RuleFor(u => u.GenderId, f => f.Random.Number(1, 4));
+
+                    var users = faker.Generate(20);
+
+                    context.Users.AddRange(users);
+
                     context.SaveChanges();
                 }
             }
+        }
+        private static string GenerateUniqueRandomRut(HashSet<string> existingRuts)
+        {
+            string rut;
+            do
+            {
+                rut = GenerateRandomRut();
+            } while (existingRuts.Contains(rut));
+
+            existingRuts.Add(rut);
+            return rut;
+        }
+
+        private static string GenerateRandomRut()
+        {
+            Random random = new();
+            int rutNumber = random.Next(1, 99999999);
+            int verificator = CalculateRutVerification(rutNumber);
+            string verificatorStr = verificator.ToString();
+            if(verificator == 10){
+                verificatorStr = "k";
+            }
+
+            return $"{rutNumber}-{verificatorStr}";
+        }
+
+        // Método para calcular el dígito verificador de un RUT
+        private static int CalculateRutVerification(int rutNumber)
+        {
+            int[] coefficients = { 2, 3, 4, 5, 6, 7 };
+            int sum = 0;
+            int index = 0;
+
+            while (rutNumber != 0)
+            {
+                sum += rutNumber % 10 * coefficients[index];
+                rutNumber /= 10;
+                index = (index + 1) % 6;
+            }
+
+            int verification = 11 - (sum % 11);
+            return verification == 11 ? 0 : verification;
         }
     }
 }
