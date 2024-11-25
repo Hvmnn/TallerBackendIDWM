fda980878d61dc0b74550cdf1e3f4d41e9fd6073
Hvmnn <felipe.hamen@alumnos.ucn.cl>
2024-11-24 23:21:16 -0300

    Eliminar DTOs y modelos obsoletos para el flujo de compras y direcciones

--- Src/DTOs/Purchase/ConfirmPurchaseDto.cs
+++
@@ -1,9 +0,0 @@
-namespace TallerBackendIDWM.Src.DTOs.Purchase
-{
-    public class ConfirmPurchaseDto
-    {
-        public required int DeliveryAddressId { get; set; }
-        public required string PaymentMethod { get; set; }
-    }
-
-}
